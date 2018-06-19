f6ff98d000d04bd78fd5797f12967a047e1a9172
Maria Nunez <mariaalician@gmail.com>
2018-06-19 23:45:25 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,6 @@ collection = ['ruby', 'javascript', 'python', 'objective-c']
 
 def my_collect(collection)
   array = [] 
-    my_collect.each do |lang|
-      lang.upcase
-    end
+  counter = 0
+   while
 end
